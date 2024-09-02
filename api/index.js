67d25f9a377c3356a491354cf6ab89e703a51ded
yangBo <1553893915@qq.com>
2024-09-02 23:57:28 +0800

    进一步完善首页

--- api/index.js
+++ api/index.js
@@ -1,3 +1,23 @@
-import request from '../utlis/request.js'
+import request from '@/utlis/request.js'
 
+/**
+ * 获取热榜帖子
+ */
 export const getTopics = () => request.get('/community/popular/topics')
+
+/**
+ * 获取标签
+ */
+export const getTags = () => request.get('/community/tags')
+/**
+ * 获取所有帖子
+ */
+export const getTopiceAll = (pageInfo, tag_id) => {
+	return request.get(`/community/topics`,{
+		params: {
+			page: pageInfo.page,
+			pageSize: pageInfo.pageSize,
+			tag_id
+		}
+	})
+}
