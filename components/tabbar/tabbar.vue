706343532de3115b802d63231d7133af19a15236
yangBo <1553893915@qq.com>
2024-09-06 00:09:57 +0800

    发布按钮可以弹出显示分类，帖子发布未完善

--- components/tabbar/tabbar.vue
+++ components/tabbar/tabbar.vue
@@ -11,10 +11,22 @@
 			</block>
 		</view>
 	</view>
+	<up-popup :show="show" mode="bottom" @close="show = false">
+			<view class="box">
+				<view class="title">请选择发布分类</view>
+				<view class="tag-list">
+				   <view v-for="item in tags" :key="item.id"> 
+					   <uni-icon name="bookmark"></uni-icon> 
+					   {{ item.tag }}
+				   </view>
+				</view>
+			</view>
+	</up-popup>
 </template>
 
 <script setup>
 	import { ref, defineProps, onMounted } from 'vue'
+	import { getTags } from '../../api/index.js'
 	
 	const props = defineProps({
 		checkedIndex: {
@@ -22,6 +34,8 @@
 		}
 	})
 	
+	const show = ref(false)
+	const tags = ref([])
 	const barList = ref([
 		{ text: '校园', src: '../../static/tabbar/shcool.png', active: '../../static/tabbar/shcool_active.png', href: '/pages/index/index' },
 		{ text: '发现', src: '../../static/tabbar/faxian.png', active: '../../static/tabbar/faxian_active.png', href: '/pages/faxian/index' },
@@ -30,16 +44,33 @@
 		{ text: '我的', src: '../../static/tabbar/user.png', active: '../../static/tabbar/user-active.png', href: '/pages/user/index' },
 	])
 	
+	onMounted(() => {
+		getTagList()
+	})
+	/**
+	 * 获取标签
+	 */
+	const getTagList = async () => {
+		const res = await getTags()
+		tags.value = res.data.data
+	} 
+	/**
+	 * 设置tabbar点击效果
+	 */
 	const checkedBar = index => {
 		// checked.value = index
-	
-		uni.switchTab({
-			url: barList.value[index].href
-		})
+		if (index === 2) {
+			show.value = true
+		} else {
+			uni.switchTab({
+				url: barList.value[index].href
+			})
+		}
+		
 	}
 </script>
 
-<style>
+<style scoped>
 	.tabbar{
 		width: 100%;
 		border-top: 1px solid #ccc;
@@ -81,5 +112,28 @@
 		padding: 20rpx;
 		border-radius: 50%;
 		background-color: #fff;
+	}
+	.box{
+		padding-bottom: 40rpx;
+	}
+	.title{
+		text-align: center;
+		margin: 30rpx 0;
+	}
+	.tag-list {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-evenly;
+	}
+	.tag-list > view {
+		width: 300rpx;
+		color: #666;
+		background-color: #e1e1e1;
+		padding: 20rpx 0 20rpx 30rpx;
+		margin-bottom: 20rpx;
+	}
+	.u-popup__content {
+		border-top-left-radius: 20rpx;
+		border-top-right-radius: 20rpx;
 	}
 </style>
